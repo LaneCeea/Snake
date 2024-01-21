a91973c2d0570cee8a843cf8b5c845a8eaeb941a
HAOHAOSIAO <huang153759@gmail.com>
2024-01-22 00:02:05 +0800

    hao hao siao update

--- Snake/src/Main.cpp
+++ Snake/src/Main.cpp
@@ -1,7 +1,9 @@
 #include <Core/Application.h>
-
+#include<iostream>
 int main() {
+    std::cout << "hello hao hao siao\n";
     auto& App = Application::GetInstance();
     App.Run();
     return 0;
+
 }
